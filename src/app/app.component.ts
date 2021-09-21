7309ac2b940546116142b87abb64e0bef06dc191
JULIÁN ANDRÉS GÓMEZ GÓMEZ <jgomez@cic.uis.edu.co>
2021-09-21 18:45:05 -0500

    Se agrega Prime NG y Google Fonts CDN

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent{
   title = 'GifApp';
+
+  dockItems: any = [];
+
+  ngOnInit() {
+      this.dockItems = [
+          {
+              label: 'Youtube',
+              icon: "../assets/Icon/youtube.svg"
+          },
+          {
+              label: 'Facebook',
+              icon: "../assets/Icon/facebook.svg"
+          },
+          {
+              label: 'Instagram',
+              icon: "../assets/Icon/instagram.svg"
+          },
+          {
+              label: 'Telegram',
+              icon: "../assets/Icon/telegrama.svg"
+          }
+      ];
+
+  }
 }
